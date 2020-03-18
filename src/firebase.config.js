587c25a6afe53c221d488a24a165db26ae2a3e64
unknown <you@example.com>
2020-03-19 00:13:00 +0600

    done

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsuz1_9W1HaCbsQihbtao0llS-ymxqbWA",
+    authDomain: "ema-john-simple-bfcf0.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-bfcf0.firebaseio.com",
+    projectId: "ema-john-simple-bfcf0",
+    storageBucket: "ema-john-simple-bfcf0.appspot.com",
+    messagingSenderId: "730929350671",
+    appId: "1:730929350671:web:fe786751c49935fa363b7a"
+  };
+
+  export default firebaseConfig;
